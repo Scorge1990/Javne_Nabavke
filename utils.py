fb98d23e36591ed527ceebd7cc8c31c5aef3e553
Scorge1990 <scorge1990@example.com>
2025-11-05 03:12:00 +0100

    Fix mapping for zakon_o_zvanicnoj_statistici to point to correct collection

--- utils.py
+++ utils.py
@@ -239,7 +239,7 @@ def map_router_to_collection(router_name: str) -> str:
         "zakon_o_zastiti_korisnika_finansijskih_usluga": "zakon_o_zastiti_korisnika_finansijskih_usluga",
         "pravilnik_o_aerosolnim_rasprasivacima": "paragraf_laws",
         "pravilnik_o_areometrima": "paragraf_laws",
-        "zakon_o_zvanicnoj_statistici": "paragraf_laws",
+        "zakon_o_zvanicnoj_statistici": "zakon_o_zvanicnoj_statistici",
         "zakon_o_regionalnom_razvoju": "zakon_o_regionalnom_razvoju",
         "zakon_o_glavnom_gradu": "zakon_o_glavnom_gradu",
         "sporazum_francuska_dvostruko_oporezivanje": "zakon_o_ratifikaciji_sporazuma_izmedju_socijalisticke_federativne_republike_jugoslavije_i_republike_francuske_o_izbegavanju_dvostrukog_oporezivanja_u_oblasti_poreza_na_dohodak_sa_protokolom",
